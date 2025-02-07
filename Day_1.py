3f2da1e74b513d852220f5aff3e110ff715cdf4b
parash <parasshah200124@gmail.com>
2025-02-06 20:14:21 -0500

    Day 1 of python learning

---
+++ Day_1.py
@@ -0,0 +1,19 @@
+#I learned how to print sentence using code in python
+print("Hello world!")
+#Learned how to add space in between the string. Typing *3 at the end of the string after
+# "closes to print that string with a number you are multipy by.
+# Adding \n will print each time in new line.
+print("Hello","","Parash!\n"*3)
+# I learned to take input and implemented it in string.
+print("hello " + input("What is your name?"))
+#Assigned each input to the variables.
+# Counted number of characteristics by typing len in front of the code.
+username= input("What is your name?")
+length= len(username)
+print(length)
+#Created project which can give you good band name where you can type input as it askes you
+# and it will return your band name.
+print("Welcome to band genrator")
+city= input("Which city you grew up in?")
+pet= input("What is your pet name?")
+print("Your Band name should be:"+ city+pet)
