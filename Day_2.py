0cdcf9854294558f5e1b45af3d0c88be30ad6104
parash <parasshah200124@gmail.com>
2025-02-08 00:32:11 -0500

    Learned to take input and calculate the input. Used previously learned concepts to achieve goal.

---
+++ Day_2.py
@@ -0,0 +1,19 @@
+#learn application of conversion.
+print(len(str(12345)))
+print(int(1234))
+print(float(1.344))
+print(bool(True))
+print(float(input("What is your hight?")))
+print(int(input("what is your age?")))
+print(str(input("What is your name?")))
+print("Number of letterns in your name: "+ str(len(input("Enter Your name"))))
+#Mathematical operation
+#print("My age: " + str(12))
+print(3*3+3/3-3)
+#change the code so output is 3
+print(3-3*3/3+3)
+#Use of f string to write variable and string togather
+bmi = 84 / 1.65 ** 2
+print(bmi)
+print(round(bmi,2))
+print(f"my is {bmi}")
